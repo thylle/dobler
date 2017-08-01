8234d28ab6e8f3c229cc957666f2e42c047d468b
Thylle <thyssen@mediaworkers.dk>
2017-08-01 23:46:21 +0200

    Cleaned up + added global helper

--- src/components/group/groups.vue
+++ src/components/group/groups.vue
@@ -1,35 +1,45 @@
 <template>
     <div class="groups">
         <h2 v-lang.group.yourGroups></h2>
-
-        <router-link class="group-list-item" 
-            :to="{path: '/' + item.Name + '/' + item.Id, params: {name: item.Name, id: item.Id}}" 
-            v-for="item in user.Groups" 
-            :key="item.Id">
-
+    
+        <router-link class="group-list-item" :to="createGroupRoute(item)" v-for="item in user.Groups" :key="item.Id">
             {{item.Name}}
         </router-link>
-
-        <button class="btn btn-primary" v-on:click="createGroup()" v-lang.group.createGroupButton></button> 
+    
+        <button class="btn btn-primary" :disabled="maxGroupsReached" v-on:click="createGroup()" v-lang.group.createGroupButton></button>
         <button class="btn" v-on:click="findNewGroup()" v-lang.group.findGroupButton></button>
+        <span v-if="maxGroupsReached" v-lang.group.maxGroupsReachedDesc></span>
     </div>
 </template>
 
 <script>
+import helpers from '../../helpers'
 
 export default {
     name: 'groups',
-    data(){
-        return{
-            user: this.$store.getters.user
-        }
-    },
-    methods:{
-        findNewGroup(){
-            this.$router.push({name: 'groupsOverview'});
+    computed: {
+        user() {
+            return this.$store.getters.user;
         },
-        createGroup(){
-            this.$router.push({name: 'createGroup'});
+        maxGroupsReached(){
+            return helpers.maxGroupsReached(this.user);
+        },
+    },
+    methods: {
+        findNewGroup() {
+            this.$router.push({ name: 'groupsOverview' });
+        },
+        createGroup() {
+            this.$router.push({ name: 'createGroup' });
+        },
+        createGroupRoute(item) {
+            return {
+                path: "/" + item.Name + "/" + item.Id,
+                params: {
+                    name: item.Name,
+                    id: item.Id
+                }
+            }
         }
     }
 }
@@ -39,11 +49,10 @@ export default {
 @import '../../../resources/custom-variables.scss';
 @import '../../../resources/custom-mixins.scss';
 
-.groups{
-    &__name{
+.groups {
+    &__name {
         margin: 0;
         font-size: $font-size-large;
     }
 }
-
 </style>
